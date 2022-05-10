d9ee7d589a7bb2665993b8879e9108fdcdc5d652
Hunc Tasci <hunc.tasci@gmail.com>
2022-05-10 23:43:19 +0300

    day3

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3Jqbp35owcqeUrnNd3GKwgoR7KOBylgg",
+  authDomain: "amzn-clone-b72e4.firebaseapp.com",
+  projectId: "amzn-clone-b72e4",
+  storageBucket: "amzn-clone-b72e4.appspot.com",
+  messagingSenderId: "43069069407",
+  appId: "1:43069069407:web:1832df0fed0a78d41323ea",
+};
